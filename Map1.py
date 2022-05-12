83b1a88ecca6fd40acb492555160f4c52c74a05f
Nazmul Hossain <nazmuljuit@gmail.com>
2022-05-12 19:08:04 +0600

    update import csv and multiple marker add

--- Map1.py
+++ Map1.py
@@ -1,9 +1,13 @@
 import folium
-map = folium.Map(location=[23.81,90.41],zoom_start=6,tiles="Stamen Terrain")
+import pandas
+data = pandas.read_csv("Volcanoes.txt")
+lon=list(data["LON"])
+lat=list(data["LAT"])
+map = folium.Map(location=[38.58,-99.09],zoom_start=6,tiles="Stamen Terrain")
 #map = folium.Map(location=[23.81,90.41],zoom_start=6,tiles="Mapbox Bright")
 fg = folium.FeatureGroup(name="My App")
-for cordinates in [[23.86,90.38],[23.60,89.83]]:
-    fg.add_child(folium.Marker(location=cordinates,popup="It is me",icon=folium.Icon(color="red")))
+for lt,ln in zip(lat,lon):
+    fg.add_child(folium.Marker(location=[lt,ln],popup="It is me",icon=folium.Icon(color="red")))
 
 map.add_child(fg)
 map.save("MapDhake.html")
