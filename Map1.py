5a3b381b1711e6a970aae0924d576361c74b2a3c
Nazmul Hossain <nazmuljuit@gmail.com>
2022-05-12 15:55:32 +0600

    multiple marker add

--- Map1.py
+++ Map1.py
@@ -1,5 +1,9 @@
 import folium
 map = folium.Map(location=[23.81,90.41],zoom_start=6,tiles="Stamen Terrain")
 #map = folium.Map(location=[23.81,90.41],zoom_start=6,tiles="Mapbox Bright")
-map.add_child(folium.Marker(location=[23.86,90.38],popup="Dhaka City",icon=folium.Icon(color="red")))
+fg = folium.FeatureGroup(name="My App")
+for cordinates in [[23.86,90.38],[23.60,89.83]]:
+    fg.add_child(folium.Marker(location=cordinates,popup="It is me",icon=folium.Icon(color="red")))
+
+map.add_child(fg)
 map.save("MapDhake.html")
