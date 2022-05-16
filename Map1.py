6e256dddda2c1e8223097ce1373d094747b8f7f4
Nazmul Hossain <nazmuljuit@gmail.com>
2022-05-16 16:53:39 +0600

    update marker details

--- Map1.py
+++ Map1.py
@@ -4,6 +4,7 @@ data = pandas.read_csv("Volcanoes.txt")
 lon=list(data["LON"])
 lat=list(data["LAT"])
 elev=list(data["ELEV"])
+
 map = folium.Map(location=[38.58,-99.09],zoom_start=6,tiles="Stamen Terrain")
 #map = folium.Map(location=[23.81,90.41],zoom_start=6,tiles="Mapbox Bright")
 fg = folium.FeatureGroup(name="My App")
