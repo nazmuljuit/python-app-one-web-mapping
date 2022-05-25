5e8755684fc2879d90af63e2c71c2fcdb6e9fea9
Nazmul Hossain <nazmuljuit@gmail.com>
2022-05-25 19:27:24 +0600

    update

--- StylingPopulationLayer.py
+++ StylingPopulationLayer.py
@@ -18,6 +18,8 @@ map = folium.Map(location=[38.58,-99.09],zoom_start=6,tiles="Stamen Terrain")
 fg = folium.FeatureGroup(name="My App")
 for lt,ln,el in zip(lat,lon,elev):
     fg.add_child(folium.CircleMarker(location=[lt,ln],radius=6,popup=str(el) + " m",fill_color=color_procedure(el),color='gray',fil_opacity=.07))
-fg.add_child(folium.GeoJson(data=(open('world.json','r',encoding='utf-8-sig').read())))
+fg.add_child(folium.GeoJson(data=open('world.json','r',encoding='utf-8-sig'),
+style_function=lambda x:{'fillColor':'green' if x['properties']['POP2005'] < 10000000 
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 map.add_child(fg)
 map.save("Map.html")
