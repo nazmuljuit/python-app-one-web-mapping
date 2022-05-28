048489c044b4da45b3013bf28b30221f286898ce
nazmuljuit <nazmuljuit@gmail.com>
2022-05-28 23:57:01 +0600

    update of last commit

--- StylingPopulationLayer.py
+++ StylingPopulationLayer.py
@@ -13,13 +13,16 @@ def color_procedure(elevation):
   else:
     return 'blue'
 
-map = folium.Map(location=[38.58,-99.09],zoom_start=6,tiles="Stamen Terrain")
+map = folium.Map(location=[38.58,-99.09],zoom_start=3,tiles="Stamen Terrain")
 #map = folium.Map(location=[23.81,90.41],zoom_start=6,tiles="Mapbox Bright")
-fg = folium.FeatureGroup(name="My App")
+fgv = folium.FeatureGroup(name="Volcanoes")
 for lt,ln,el in zip(lat,lon,elev):
-    fg.add_child(folium.CircleMarker(location=[lt,ln],radius=6,popup=str(el) + " m",fill_color=color_procedure(el),color='gray',fil_opacity=.07))
-fg.add_child(folium.GeoJson(data=open('world.json','r',encoding='utf-8-sig'),
-style_function=lambda x:{'fillColor':'green' if x['properties']['POP2005'] < 10000000 
+    fgv.add_child(folium.CircleMarker(location=[lt,ln],radius=6,popup=str(el) + " m",fill_color=color_procedure(el),color='gray',fil_opacity=.07))
+fgp = folium.FeatureGroup(name="Populations")
+
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(), style_function=lambda x:{'fillColor':'green' if x['properties']['POP2005'] < 10000000 
 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
-map.add_child(fg)
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 map.save("Map.html")
